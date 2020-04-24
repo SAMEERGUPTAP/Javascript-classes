d0b3ac329597fa78b0579c5a0a8997134f3aa6fb
sameer <sameer.webdesigner456@gmail.com>
2020-04-24 22:55:04 +0530

    first javascript class

---
+++ custom.js
@@ -0,0 +1,45 @@
+
+// topic
+// 1. variable 
+// 2. DataType
+// 3. operators 
+
+
+
+// 1. primitave data
+//   1. string
+//   2. number
+//   3. boolean
+//   4. null
+//   5. undefined
+
+
+// 1. AirthMatics
+   // +
+   // -
+   // *
+   // /
+   //  %
+   // ++
+   //  --
+   // **
+
+   
+
+var firstName = 'Sameer'  //string
+var x =  4545  // number   
+
+// typeof are used to check dataType of primitive data
+
+var c = 'shsghsdg' + '' + 'dsbdjsd' + ' ' + 10;
+
+var y = 10;
+var z = 10;
+var x = '5';
+
+var t = y + z + +x;
+console.log(t) 
+// var sum = 10 + '5' + 10;
+
+// console.log(sum)
+
